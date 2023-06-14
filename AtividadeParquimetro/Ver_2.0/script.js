1b2dcffb3d3bfce410a14e832b333b00c632a4e7
esquilo korte <dudu.korte@gmail.com>
2023-06-14 19:55:10 -0300

    Add files via upload

--- AtividadeParquimetro/Ver_2.0/script.js
+++ AtividadeParquimetro/Ver_2.0/script.js
@@ -3,22 +3,26 @@ function addTime(cost) {
     var actualMoney = Number(document.getElementById("idMoney").value)
     var outTime = document.getElementById("outTime")
     var outAlarm = document.getElementById("outAlert")
+    var actualTime = Number(document.getElementById("outActualTime").textContent)
+    var outActualTime = document.getElementById("outActualTime")
 
     //switch case para cada caso, se for custo de 1 real, 1,75 etc...
     if (!(actualMoney - cost < 0)) {
         switch (cost) {
             case 1:
-                timeToAdd = 30
+                var timeToAdd = 30
                 break
             case 1.75:
-                timeToAdd = 60
+                var timeToAdd = 60
                 break
             case 3:
-                timeToAdd = 120
+                var timeToAdd = 120
                 break
         }
         outAlarm.className = "alert alert-danger visually-hidden"
         outTime.innerText = `Troco: ${actualMoney - cost}`
+        actualTime += timeToAdd
+        outActualTime.innerText = actualTime
     } else { //caso não tenha dinheiro suficiente na carteira
         outAlarm.className = "alert alert-danger"
         outTime.innerText = ""
